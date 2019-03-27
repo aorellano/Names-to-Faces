04e48bbc92c710f76f2d25a88cfd3cdb2632905e
Alexis Orellano <alexisorellano@Andrews-MacBook-Pro-2.local>
2019-03-26 20:36:24 -0500

    first commit

---
+++ Project10/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Alexis Orellano on 3/26/19.
+//  Copyright © 2019 Alexis Orellano. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String){
+        self.name = name
+        self.image = image
+    }
+
+}
---
+++ Project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Alexis Orellano on 3/26/19.
+//  Copyright © 2019 Alexis Orellano. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
